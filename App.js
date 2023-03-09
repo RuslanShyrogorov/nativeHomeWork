05177ac24daf98a06d0a4768b888d5c98ac2c2a5
RuslanShyrogorov <ruslanshirogorov@gmail.com>
2023-03-10 00:26:39 +0200

    hw-2

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+
+import { useState, useEffect, useCallback } from "react";
+
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+SplashScreen.preventAutoHideAsync();
+
+import RegistrationScreen from "./components/RegistrationScreen";
+import LoginScreen from "./components/LoginScreen";
 
 export default function App() {
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [isRegister, setIsRegister] = useState(true);
+
+  const keyboardHide = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+  };
+
+  useEffect(() => {
+    Keyboard.addListener("keyboardWillShow", () => {
+      setIsShowKeyboard(true);
+    });
+    Keyboard.addListener("keyboardWillHide", () => {
+      setIsShowKeyboard(false);
+      Keyboard.dismiss();
+    });
+    return () => {
+      Keyboard.removeAllListeners;
+    };
+  }, []);
+
+  // FONTS
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <View style={styles.container} onLayout={onLayoutRootView}>
+        <ImageBackground
+          style={styles.image}
+          source={require("./assets/images/photo-bg.png")}
+        >
+          {isRegister ? (
+            <RegistrationScreen
+              setIsShowKeyboard={setIsShowKeyboard}
+              isShowKeyboard={isShowKeyboard}
+              setIsRegister={setIsRegister}
+            />
+          ) : (
+            <LoginScreen
+              setIsShowKeyboard={setIsShowKeyboard}
+              isShowKeyboard={isShowKeyboard}
+              setIsRegister={setIsRegister}
+            />
+          )}
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    color: "#212121",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
   },
 });
